a91fc4558b7dd13f4f18bafe9eb3389768f3aca6
Muntaser Muttaqi <muntasermuttaqi@gmail.com>
2022-12-10 16:23:19 +0600

    initial phase

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,5 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2020, 12-1, 10, 8)}); // year, month, date, hour
-        });
-});		
-
+jQuery(document).ready(function () {
+  $(function () {
+    $("#defaultCountdown").countdown({ until: new Date(2020, 12 - 1, 10, 8) }); // year, month, date, hour
+  });
+});
